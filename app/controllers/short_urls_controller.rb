de2e01050c7768a11fc5e1c82e7c8b9aeb45cecd
David Bayendor <dbayendor@gmail.com>
2015-02-05 17:35:42 -0700

    Clean up controller

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,74 +1,47 @@
 class ShortUrlsController < ApplicationController
-  before_action :set_short_url, only: [:show, :edit, :update, :destroy]
+  before_action :set_short_url, only: [:show]
 
-  # GET /short_urls
-  # GET /short_urls.json
-  def index
-    @short_urls = ShortUrl.all
-  end
-
-  # GET /short_urls/1
-  # GET /short_urls/1.json
   def show
+    @short = set_short_url
+
+    respond_to do |format|
+      format.html { redirect_to @short.original_url }
+      format.json { render :show, status: :ok, location: @short_url }
+    end
   end
 
-  # GET /short_urls/new
   def new
+    @short_urls = ShortUrl.all
     @short_url = ShortUrl.new
+
+    respond_to do |format|
+      format.html
+    end
   end
 
-  # GET /short_urls/1/edit
-  def edit
-  end
-
-  # POST /short_urls
-  # POST /short_urls.json
   def create
     @short_url = ShortUrl.new(short_url_params)
 
     respond_to do |format|
       if @short_url.save
-        format.html { redirect_to @short_url, notice: "Short url was successfully created." }
+        format.html { redirect_to @short_url, notice: "Short url was created." }
         format.json { render :show, status: :created, location: @short_url }
       else
         format.html { render :new }
-        format.json { render json: @short_url.errors, status: :unprocessable_entity }
+        format.json do
+          render json: @short_url.errors,
+                 status: :unprocessable_entity
+        end
       end
-    end
-  end
-
-  # PATCH/PUT /short_urls/1
-  # PATCH/PUT /short_urls/1.json
-  def update
-    respond_to do |format|
-      if @short_url.update(short_url_params)
-        format.html { redirect_to @short_url, notice: "Short url was successfully updated." }
-        format.json { render :show, status: :ok, location: @short_url }
-      else
-        format.html { render :edit }
-        format.json { render json: @short_url.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /short_urls/1
-  # DELETE /short_urls/1.json
-  def destroy
-    @short_url.destroy
-    respond_to do |format|
-      format.html { redirect_to short_urls_url, notice: "Short url was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_short_url
     @short_url = ShortUrl.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def short_url_params
     params.require(:short_url).permit(:original_url)
   end
