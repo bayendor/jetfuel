bc342852146fb302c576230a4eb657c31558d3c0
David Bayendor <dbayendor@gmail.com>
2015-02-05 17:34:17 -0700

    Get all tests passing before moving on

--- spec/requests/short_urls_spec.rb
+++ spec/requests/short_urls_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "ShortUrls", type: :request do
   describe "GET /short_urls" do
-    it "works! (now write some real specs)" do
+    it "lists urls" do
       get short_urls_path
       expect(response).to have_http_status(200)
     end
