6e13b2927b9ddc391440860949f59f7c11e2cd64
David Bayendor <dbayendor@gmail.com>
2015-02-07 19:08:16 -0700

    Add a visits column to model

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204215237) do
+ActiveRecord::Schema.define(version: 20150208002654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "short_urls", force: :cascade do |t|
     t.string   "original_url"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "visits",       default: 0
   end
 
 end
